e0ce27cae101a2dd1586ee7c4771fd785c02e98e
rojasevernahuel-maker <rojasevernahuel@gmail.com>
2026-02-12 23:21:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const clientes = {
-  "EV1503JULI": {
-    nombre: "15 años de Juli"
+  "MARIA2026": {
+    nombre: "XV - María",
+    fecha: "2026-02-14",
+    ubicacion: "La Florida",
+    combo: "Álbum grande - 100 fotos",
+    incluye: [
+      "📸 Todas las fotos digitales",
+      "📘 1 álbum grande con 100 fotos",
+      "📷 1 sesión fotográfica",
+      "🖼️ 1 portarretrato",
+      "🚁 Dron en evento",
+      "🧧 Tarjeta virtual",
+      "🖨️ 10 fotos polaroid"
+    ],
+    total: 750000,
+    pagado: 750000,
+    extras: [
+      {
+        nombre: "Sesión pre entrada",
+        descripcion: "30 minutos antes de ingresar al salón para sesión especial",
+        precio: 50000
+      },
+      {
+        nombre: "Filmación con dron en la sesión",
+        descripcion: "Tomas aéreas antes del ingreso al salón",
+        precio: 50000
+      }
+    ]
   }
 };
 
@@ -8,7 +34,7 @@ function verificarCodigo() {
   const codigo = document.getElementById("codigo").value.toUpperCase();
   
   if (clientes[codigo]) {
-    window.location.href = "evento.html";
+    window.location.href = `evento.html?codigo=${codigo}`;
   } else {
     document.getElementById("error").textContent = "Código inválido";
   }
